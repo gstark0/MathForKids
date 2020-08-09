18394988fd99401238e78aafc736398a9f6dcbb7
gstark0 <gstark0@icloud.com>
2020-08-09 12:20:30 +0200

    Change name of the variable

--- MathForKids/ContentView.swift
+++ MathForKids/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     @State private var maxRange = 6
     @State private var numOfQuestions = 5
-    @State private var isSetting = true
+    @State private var isPlaying = false
     
     var body: some View {
         NavigationView {
@@ -25,7 +25,9 @@ struct ContentView: View {
                     .shadow(color: Color.init(red: 0, green: 0, blue: 0, opacity: 0.06), radius: 0, x: 0, y: 8)
                     .padding(.bottom, 40)
                 
-                if isSetting {
+                if isPlaying {
+                    
+                } else {
                     Stepper(value: $maxRange, in: 2...12) {
                         Text("Range of numbers: \(self.maxRange)")
                     }
@@ -37,8 +39,8 @@ struct ContentView: View {
                 }
                 Spacer()
                 
-                Button(isSetting ? "Start" : "Submit") {
-                    self.isSetting = false
+                Button(isPlaying ? "Submit" : "Start") {
+                    self.isPlaying = true
                 }
                     .font(.system(size: 24, weight: .bold))
                     .foregroundColor(.white)
