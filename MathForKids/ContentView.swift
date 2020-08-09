4fbe1a8ba69e2243045779a0b7208487e956aae3
gstark0 <gstark0@icloud.com>
2020-08-09 12:18:43 +0200

    Add a basic layout

--- MathForKids/ContentView.swift
+++ MathForKids/ContentView.swift
@@ -9,8 +9,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var maxRange = 6
+    @State private var numOfQuestions = 5
+    @State private var isSetting = true
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                Spacer()
+                Image("pig")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 110)
+                    .shadow(color: Color.init(red: 0, green: 0, blue: 0, opacity: 0.06), radius: 0, x: 0, y: 8)
+                    .padding(.bottom, 40)
+                
+                if isSetting {
+                    Stepper(value: $maxRange, in: 2...12) {
+                        Text("Range of numbers: \(self.maxRange)")
+                    }
+                        .padding(.horizontal)
+                    Stepper(value: $numOfQuestions, in: 4...30) {
+                        Text("Number of questions: \(self.numOfQuestions)")
+                    }
+                        .padding(.horizontal)
+                }
+                Spacer()
+                
+                Button(isSetting ? "Start" : "Submit") {
+                    self.isSetting = false
+                }
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.init("Basic"))
+                    .cornerRadius(5)
+                    .padding(.horizontal)
+                    .padding(.bottom, 30)
+            }
+            .navigationBarTitle("MathForKids", displayMode: .inline)
+        }
     }
 }
 
